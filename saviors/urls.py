064ac7b0ede9c802c33078bebbeb95cac3325995
Eduardo Júnio <eduardojvr10@gmail.com>
2018-04-20 01:33:35 -0300

    Adicionando página inicial (index.html)

--- saviors/urls.py
+++ saviors/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from saviors import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
