dcf75a1e13947a395e85f0a198ad39e47af5be50
amandabezerra <amandabs_contato@hotmail.com>
2018-05-02 04:29:56 -0300

    Create login system - Update README - Update base templates

--- saviors/urls.py
+++ saviors/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from saviors import views
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     url(r'^$', views.index, name='index'),
 ]
