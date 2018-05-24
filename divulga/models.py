7cb6efe24e4ffefd77521908fc66043149e3e1c2
Eduardojvr <eduardojvr10@gmail.com>
2018-05-23 22:31:18 -0300

    Atualização de model e formulário de evento

--- divulga/models.py
+++ divulga/models.py
@@ -9,4 +9,8 @@ class Divulgacoes(models.Model):
     bairro = models.CharField(max_length=140, null=False)
     endereco = models.CharField(max_length=140, null=False)
     cep = models.CharField(max_length=140, null=False)
+    horarioInicio = models.CharField(max_length=20, null=False)
+    horarioFim = models.CharField(max_length=20, null=False)
+    data = models.CharField(max_length=20, null=False)
+    #data = models.DateTimeField(auto_now=False, auto_now_add=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- divulga/views.py
+++ divulga/views.py
@@ -12,6 +12,10 @@ def formevent(request):
         divulgacao.endereco = request.POST['endereco']
         divulgacao.cep = request.POST['cep']
         divulgacao.telefone = request.POST['telefone']
+        divulgacao.horarioInicio = request.POST['horarioInicio']
+        divulgacao.horarioFim = request.POST['horarioFim']
+        divulgacao.data = request.POST['data']
         divulgacao.user = request.user
         divulgacao.save()
+        return render(request , 'index.html')
     return render(request , 'formevent.html')
