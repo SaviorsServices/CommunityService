55e96e4278aa7ccd0b03471fe52f738f4152b4ef
ulysses <ulysses3353@gmail.com>
2018-05-26 16:25:20 -0300

    Passing class diagram to code

--- divulga/views.py
+++ divulga/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 from .models import Divulgacoes
 from django.contrib.auth.decorators import login_required
+from .forms import CommunityActionForm, DonationForm, HealthServiceForm, EstablishmentForm 
 
 @login_required
 def formevent(request):
@@ -30,3 +35,45 @@ def servicolist(request):
     div = Divulgacoes.objects.all()
     return render(request, 'servicolist.html', {'div':div})
 
+
+def create_establishment(request):
+    title = "Cadastrar Estabelecimento"
+    if request.method == "POST":
+        form = EstablishmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        return render(request, 'establishment.html', {'form': form,'title':title})
+
+    else:
+        form = EstablishmentForm()
+        return render(request, 'establishment.html', {'form': form,'title':title})
+
+
+from .forms import CommunityActionForm, DonationForm, HealthServiceForm, EstablishmentForm 
+    
+def create_health_service(request):
+    title = "Cadastrar Serviço de Saude"
+    if request.method == "POST":
+        form = HealthServiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        return render(request, 'establishment.html', {'form': form,'title':title})
+
+    else:
+        form = HealthServiceForm()
+        return render(request, 'establishment.html', {'form': form,'title':title})
+
+def create_donation(request):
+    title = "Cadastrar Doação"
+    if request.method == "POST":
+        form = DonationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        return render(request, 'establishment.html', {'form': form,'title':title})
+
+    else:
+        form = DonationForm()
+        return render(request, 'establishment.html', {'form': form,'title':title})
